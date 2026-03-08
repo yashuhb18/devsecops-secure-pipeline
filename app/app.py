cfc892bc558946e8d09614b95eafbb93a9c2f5d0
Yash <yashuhb18@gmail.com>
2026-03-08 16:47:44 +0530

    add secret vulnerability for testing

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+
+# ❌ Hardcoded secret vulnerability
+API_KEY = "sk_live_SECRET_KEY_123456"
 
 @app.route("/")
 def home():
     return "DevSecOps Secure Pipeline Running!"
+
+@app.route("/secret")
+def secret():
+    return f"API KEY: {API_KEY}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
